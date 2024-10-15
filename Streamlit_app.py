8a379b7a3d8b02418a11ce02997df08a913ab73a
Florian-Mtrt <matratflorian@gmail.com>
2024-10-15 11:59:25 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 df_github = pd.read_csv('owid-co2-data.csv', header=0)
 df_GLB_NASA = pd.read_csv('GLB.Ts+dSST.csv', header=1, index_col=1)
